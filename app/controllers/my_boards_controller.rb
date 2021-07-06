2b05c0e4b7b330e98c9e0ff5fe6cb6d50451584f
Levy Cen <levycen.d@gmail.com>
2021-07-06 03:48:09 -0500

    Add mines to board and set values fot all squards

--- app/controllers/my_boards_controller.rb
+++ app/controllers/my_boards_controller.rb
@@ -24,12 +24,14 @@ class MyBoardsController < ApplicationController
     #POST /myboards
     def create
         @my_board = Board.new(board_params)
-        @mines = 3
         if @my_board.save
             create_square(@my_board.height,
                         @my_board.width,
                         @my_board.id)
+            
             add_mines(@my_board.id,@my_board.mines,)
+
+            set_squares_values(@my_board)
 
             render json:@my_board, status: :ok
         else
@@ -100,7 +102,7 @@ class MyBoardsController < ApplicationController
         board_squares = Square.where(board_id: board_id)
         
         mines_added = 0
-        num_max = board_squares.size
+        num_max = board_squares.size-1
         while mines_added < number_mines
             square_mine = rand(0..num_max)
             unless board_squares[square_mine].mine
@@ -111,4 +113,73 @@ class MyBoardsController < ApplicationController
         
     end
 
+    #set values to squares
+    def set_squares_values(board)
+        board_id = board.id
+        board_height = board.height
+        board_width = board.width
+
+        boards_squares = Square.where(board_id: board_id, mine: false)
+        all_boards_squares = Square.where(board_id: board_id)
+        
+        board_mines = Square.where(board_id: board_id, mine: true)
+
+        for k in 0..board_mines.size-1 do puts "minas en #{board_mines[k].row} #{board_mines[k].column} " end
+            
+        for iteration in 0..boards_squares.size-1 do
+            coordinates_list = get_coordinates_arround(board_height,
+                board_width,
+                boards_squares[iteration].row,
+                boards_squares[iteration].column)
+            square_value = generate_square_value(all_boards_squares,coordinates_list)
+            boards_squares[iteration].update(value: square_value)
+            puts "Coordenada [#{boards_squares[iteration].row},#{boards_squares[iteration].column}] se encontró el valor #{square_value} y se guardo #{boards_squares[iteration].value}"
+        end
+
+    end
+
+    def get_coordinates_arround(board_height,board_width,row,column)
+
+        row_less = row - 1
+        row_higher = row + 1
+        column_less = column - 1
+        column_higher = column + 1
+
+        coordinates_list = Array.new()
+
+        for row_ in row_less..row_higher do
+            for column_ in column_less..column_higher do
+                coo_list = Array.new()
+                if column_ == column && row_ == row
+                    #no add
+                elsif row_ < 0 || column_ < 0
+                    # fuera de tablero por arriba y por izquierda
+                    #no add
+                elsif  row_ >= board_height || column_ >= board_width
+                    # fuera de tablero por abajo y por la dercha
+                    #no add
+                else 
+                    coo_list.push(row_)
+                    coo_list.push(column_)
+                end
+
+                unless coo_list.empty?
+                    coordinates_list.push(coo_list)
+                end
+            end
+        end
+        puts coordinates_list.to_s
+        return coordinates_list
+    end
+
+    # 
+    def generate_square_value(boards_squares,coordinates_list)
+        count_mines = 0
+        for i in 0..(coordinates_list.size-1) do
+            if boards_squares.find{|b| b.row == coordinates_list[i][0] && b.column == coordinates_list[i][1] }.mine
+                count_mines += 1
+            end
+        end
+        return count_mines
+    end 
 end
