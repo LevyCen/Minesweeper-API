1772b9f0c5f21f4674b631c6a484060d01566904
Levy Cen <levycen.d@gmail.com>
2021-07-11 05:02:40 -0500

    Refactoring method POST in Square class

--- app/controllers/squares_controller.rb
+++ app/controllers/squares_controller.rb
@@ -1,8 +1,8 @@
 class SquaresController < ApplicationController
 
-    rescue_from Exception do |e|
-        render json: {error: e.message}, status: :internal_server_error
-    end
+    # rescue_from Exception do |e|
+    #     render json: {error: e.message}, status: :internal_server_error
+    # end
   
     rescue_from ActiveRecord::RecordInvalid do |e|
         render json: {error: e.message}, status: :unprocessable_entity
@@ -16,7 +16,7 @@ class SquaresController < ApplicationController
     def open_box
         # recuperar el tablero
         my_boards_squares =  Square.where(board_id: params[:board_id])
-        break_flag = false
+        flag_is_ok = true
         
         #validar las coordenadas son minas?
         if my_boards_squares.where(row: params[:row], column: params[:column]).first.mine
@@ -42,67 +42,45 @@ class SquaresController < ApplicationController
 
         else
             # si no son minas, continuamos
-            if my_boards_squares.where(row: params[:row], column: params[:column]).first.open
+            square_open =  my_boards_squares.where(row: params[:row], column: params[:column]).first.open
+            square_value = my_boards_squares.where(row: params[:row], column: params[:column]).first.value 
+
+            if square_open
                 # las coordenadas son de una caja abierta? si son. entonces notificar caja abierta
                 return render json: {
                     status: 'fail',
                     message: 'box was open',
                     data: my_boards_squares
                 },status: :not_acceptable
-                break_flag = true
+                flag_is_ok = false
                 
-            elsif my_boards_squares.where(row: params[:row], column: params[:column]).first.value > 0
+            elsif square_value > 0
                 # si no, las coordenadas son de una caja con valor > a 0 ? si son. abrimos la caja
-                my_boards_squares.where(row: params[:row], column: params[:column]).first.update({"open"=>true})
-                
-            elsif my_boards_squares.where(row: params[:row], column: params[:column]).first.value = 0
+                # = SquaresService(my_board)
+                a = SquaresService.new()
+                a.open_square(my_boards_squares,params[:row],params[:column])
+
+            elsif square_value == 0
                 # si no, las coordenadas son de una caja con valor = 0 ? si son. abrimos la caja 
                 # y abrimos las cajas de al rededor con valor mayor a 0,
-                my_board = Board.find(params[:board_id]) 
-                value_zero_list = Array.new()
-                value_zero_list.push(my_boards_squares.where(row: params[:row], column: params[:column]).first)
-                array_position = 0
-                
-                while array_position < value_zero_list.size do
-                    
-                    value_zero_list[array_position].update({"open"=>true})
-                    coordinates_list = CoordinatesService.get_coordinates_arround(my_board.height, my_board.width, value_zero_list[array_position].row, value_zero_list[array_position].column)
-
-
-                    for value in 0..coordinates_list.size-1 do
-                        if my_boards_squares.where(row: coordinates_list[value][0], column: coordinates_list[value][1]).first.value > 0
-                            # open box
-                            unless my_boards_squares.where(row: coordinates_list[value][0], column: coordinates_list[value][1]).first.open
-                                my_boards_squares.where(row: coordinates_list[value][0], column: coordinates_list[value][1]).first.update({"open"=>true})
-                            end
-                        else
-                            # save news box with value = 0
-                            # if there are another box whit value = 0 then, apply the same procedure
-                            unless my_boards_squares.where(row: coordinates_list[value][0], column: coordinates_list[value][1]).first.open
-                                my_boards_squares.where(row: coordinates_list[value][0], column: coordinates_list[value][1]).first.update({"open"=>true})
-                                value_zero_list.push(my_boards_squares.where(row: coordinates_list[value][0], column: coordinates_list[value][1]).first)
-                            end
-                        end                   
-                    end
-                    array_position += 1
-                end
-                
+                a = SquaresService.new()
+                a.open_square_value_zero(my_boards_squares,params[:row],params[:column])
 
             else
                 #error con las coordenas enviadas
                 return render json: {
                     status: 'fail',
-                    message: 'error with box',
+                    message: 'error with square',
                     data: my_boards_squares
                 },status: :not_acceptable
-                break_flag = true
+                flag_is_ok = false
             end
 
-            if break_flag
-            #return message
-            else
-                # al final se evalua si todas las que no son minas ya están abiertas
-                if my_boards_squares.where(mine: false, open: false).empty?
+
+            if flag_is_ok
+                # se evalua si todas las que no son minas ya están abiertas
+
+                if game_is_finised(my_boards_squares)
                     #player won
                     render json: {
                         status: 'successful',
@@ -110,7 +88,7 @@ class SquaresController < ApplicationController
                         data: my_boards_squares
                     },status: :ok
                 else
-                    #player won
+                    #player continue
                     render json: {
                         status: 'continue',
                         message: 'nice continue playing',
@@ -121,13 +99,14 @@ class SquaresController < ApplicationController
 
         end
         
+    end
 
-        
-        
-        
-        
-        
+    def game_is_finised(my_boards_squares)
+        if my_boards_squares.where(mine: false, open: false).empty?
+            return true
+        else
+            return false
+        end
 
-        
     end
 end
